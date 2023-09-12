265bfa28632f7c61705456822a4317bb4b89d0a3
James Moore <james3.moore@dish.com>
2023-09-12 16:47:38 -0600

    ~ Updated player and main files

--- main.java
+++ main.java
@@ -1,32 +1,59 @@
 import java.util.Scanner;
 
 class Main {
+  // Count used to access player inputs
+  private static int setupCount = 0;
+
   public static void main(String[] args) {
-    // Create player one's instance
+    // Create player's instances
     Player playerOne = new Player();
+    Player playerTwo = new Player();
     Scanner playerOneInput = new Scanner(System.in);
+    Scanner playerTwoInput = new Scanner(System.in);
+
+    // Arrays to hold players and their scanners
+    Player[] players = { playerOne, playerTwo };
+    Scanner[] playerInputs = { playerOneInput, playerTwoInput };
+
+    // Aliases
+    Player currentPlayer = players[setupCount];
+    Scanner currentPlayerInput = playerInputs[setupCount];
 
     // Welcome Message
     System.out.println("Welcome to my Tic Tac Toe game! \nPlease enter Player One's Name then Player Two");
 
-    // Prompt Player for name
-    System.out.print("Player One's name: ");
+    while (!playerOne.getIsReady() && !playerTwo.getIsReady()) {
 
-    // Read a text entered by Player 1 and set the name of player
-    playerOne.setName(playerOneInput.nextLine());
+      // Prompt player for name, +1 to account for 0 indexing
+      System.out.print("Player" + (setupCount == 0 ? " 1 " : " 2 ") + "name: ");
 
-    // Prompt Player for their choice of X or O
-    System.out.print("Player One's choice: ");
+      // Read a text entered by Player 1 and set the name of player
+      currentPlayer.setName(currentPlayerInput.nextLine());
 
-    while (!"X".equals(playerOne.getChoice()) && !"O".equals(playerOne.getChoice())) {
-      System.out.print("Please choose 'X' or 'O': ");
-      String input = playerOneInput.nextLine();
-      playerOne.setChoice(input);
+      // Prompt player for their choice of X or O
+      while (!currentPlayer.getChoice().equals("X") && !currentPlayer.getChoice().equals("O")) {
+        System.out.print("Player" + (setupCount == 0 ? " 1 " : " 2 ") + "choice (Please choose 'X' or 'O'): ");
+        currentPlayer.setChoice(currentPlayerInput.nextLine());
+        currentPlayer.setIsReady(true);
+      }
+
+      // Flipping setup count between 0 and 1
+      if (setupCount == 0) {
+        setupCount = 1;
+      } else {
+        setupCount = 0;
+      }
+
+      System.out.println(playerOne.getIsReady());
+      System.out.println(playerTwo.getIsReady());
     }
 
     // Close scanner to avoid leak
-    playerOneInput.close();
 
-    System.out.println(playerOne.getName());
+    playerInputs[0].close();
+    playerInputs[1].close();
+
+    System.out.println("player one is ready? " + playerOne.getIsReady());
+    System.out.println("player two is ready? " + playerTwo.getIsReady());
   }
 }
--- player.java
+++ player.java
@@ -2,11 +2,13 @@ class Player {
   // Basic info of player
   private String name;
   private String choice;
+  private Boolean isReady;
 
   // Public constructor
   public Player() {
     this.name = "";
     this.choice = "";
+    this.isReady = false;
   }
 
   // Getter Methods
@@ -18,6 +20,10 @@ class Player {
     return choice;
   }
 
+  public Boolean getIsReady() {
+    return isReady;
+  }
+
   // Setter Methods
   public void setName(String name) {
     this.name = name;
@@ -25,5 +31,9 @@ class Player {
 
   public void setChoice(String choice) {
     this.choice = choice;
+  }
+
+  public void setIsReady(Boolean isReady) {
+      this.isReady = isReady;
   }
 }
