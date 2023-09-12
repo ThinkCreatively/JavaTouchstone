c7c33c5c9308ccae475577befea9ed2997aeccc8
James Moore <james3.moore@dish.com>
2023-09-12 15:32:05 -0600

    ~ Created welcome message and  player one's creation

--- main.java
+++ main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    int num1 = 5;
-    int num2 = 8;
-    System.out.println(num1 + num2 * num1);
+    Scanner playerOneInput = new Scanner(System.in);
+
+    // Welcome Message
+    System.out.println("Welcome to my Tic Tac Toe game! \nPlease enter Player One's Name then Player Two");
+
+    // Prompt Player for name
+    System.out.print("Player One's name: ");
+
+    // Read a text entered by Player 1
+    String playerOneName = playerOneInput.nextLine();
+
+    // Prompt Player for name
+    System.out.print("and Player One's choice: ");
+
+    // Read a text entered by Player 1
+    String playerOneChoice = playerOneInput.nextLine();
+
+    // Create player one's instance
+    Player playerOne = new Player(playerOneName, playerOneChoice);
+
+    // Close scanner to avoid leak
+    playerOneInput.close();
+
+    System.out.println(playerOne.getName());
   }
 }
