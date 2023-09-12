7e5fb3263bc84daf147d1e8442dd69b212c093bc
James Moore <james3.moore@dish.com>
2023-09-12 14:34:13 -0600

    Initial comit

---
+++ main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 8;
+    System.out.println(num1 + num2 * num1);
+  }
+}
---
+++ player.java
@@ -0,0 +1,11 @@
+class player {
+ private String name;
+ 
+  
+  public Dog(String name, String breed, int age, String color) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.color = color;
+  }
+}
